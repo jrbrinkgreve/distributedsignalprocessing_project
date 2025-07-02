a75374d85bb9dca5e516da6558bfb0473af7ebf9
jrbrinkgreve <152167305+jrbrinkgreve@users.noreply.github.com>
2025-07-02 12:05:35 +0200

    updated plots

--- main.m
+++ main.m
@@ -1,15 +1,12 @@
 clc; clear; close all;
 
-
-%range variable, num sensors fixed
-
-
+% fixing range of sensors, calculating communication range
 area_length = 100; % in meters => area = 100 x 100 sqm
-n = 20:5:200; % number of (potential) sensors deployed in area
+n = 20:5:200; % number of (potential) sensors deployed in RGG
 r = zeros(size(n));
 
 for i = 1:length(n)
-    r(i) = sqrt(log(n(i)) / (pi * n(i))) * area_length;
+    r(i) = sqrt(2 * log(n(i)) / (n(i))) * area_length;
 end
 
 figure;
@@ -19,19 +16,14 @@ ylabel("Communication radius (in meters)");
 title("Tradeoff in sensor deployment");
 grid on;
 
-
-
-
-%range constant, num sensors variable
-
-
+% let us now assume a constant radius, say 50 m
 comm_radius = 20;
-num_trials = 1000; % number of Monte Carlo trials
+num_trials = 200; % number of Monte Carlo trials
 % min_sensors = inf; % worst-case (unrealistic) scenario
 
 connectivity_prob = zeros(size(n));
 
-parfor k = 1:length(n)
+for k = 1:length(n)
     success = 0;
 
     for t = 1:num_trials
@@ -61,7 +53,6 @@ parfor k = 1:length(n)
     connectivity_prob(k) = success / num_trials;
 end
 
-%plotting results
 figure;
 plot(n, connectivity_prob, '-o', LineWidth=2);
 xlabel('Number of sensors deployed');
@@ -69,18 +60,16 @@ ylabel('Connectivity probability');
 title(sprintf('Connectivity vs. Sensors (r = %d, trials = %d)', comm_radius, num_trials));
 grid on;
 
-
-%------------------------------------------------------------------------------------------------
-
-
-
 % from above two graphs, taking comm_radius = 20, num_sensors = 100
+% increasing sensors by 5 to show good convergence in median consensus
+% using same graph
 % because there is a very high probability of connectivity
 
-num_sensors = 101;
+num_sensors = 100;
+% num_sensors = 105;
 sensor_positions = area_length * rand(num_sensors, 2);
 
-%construct adjacency matrix
+% adjacency matrix
 adjacency_matrix = zeros(num_sensors);
 for i = 1:num_sensors
     for j = i+1:num_sensors
@@ -116,21 +105,15 @@ if all(conncomp(sensor_network) == 1)
     title(sprintf('Random Geometric Graph (r = %d, n = %d)', comm_radius, num_sensors));
 end
 
-
-
-% parameters per sensor
+%% parameters per sensor
 actual_value = 30;
 reading_error = 2; % reading error
 x_initial = reading_error .* randn(num_sensors, 1) + actual_value; % initial values
 true_avg = mean(x_initial);
-true_median = median(x_initial);
-max_iterations = 2e5; % safety cap on iterations
+max_iterations = 200000; % safety cap on iterations
 tolerance = 1e-6; % convergence tolerance
 
-
-
-%---------------------------------------------------------------------------------
-% randomized gossip implementation (average consensus)
+%% randomized gossip implementation (average consensus)
 x_gossip = x_initial;
 % x_gossip_history = zeros(max_iterations, num_sensors);
 error_history_gossip = zeros(max_iterations, 1);
@@ -142,6 +125,13 @@ for iter_gossip = 1:max_iterations
 
     % convergence check
     if iter_gossip > 1 && norm(x_gossip - true_avg) < tolerance
+        convergence_time_gossip = iter_gossip;
+        error_history_gossip = error_history_gossip(1:iter_gossip, :);
+        break;
+    end
+
+    if iter_gossip > 1 && norm(x_gossip - true_avg) > 1e3
+        fprintf("Graph diverging!!!");
         convergence_time_gossip = iter_gossip;
         error_history_gossip = error_history_gossip(1:iter_gossip, :);
         break;
@@ -172,11 +162,14 @@ if iter_gossip == max_iterations
     fprintf("WARNING: Maximum iterations reached without convergence!\n");
 end
 
+trans_per_iter = 2;
+transmission_axis_gossip = trans_per_iter * (1:length(error_history_gossip));
+
 % plotting convergence
 figure;
-semilogy(1:size(error_history_gossip,1), error_history_gossip);
+semilogy(transmission_axis_gossip, error_history_gossip);
 title('Randomized Gossip Convergence (Average Consensus)');
-xlabel('Iterations'); ylabel('MSE w.r.t. True Average');
+xlabel('Transmissions'); ylabel('MSE w.r.t. True Average');
 grid on;
 
 fprintf('Gossip (Average Consensus) - Convergence time: %d iterations, Transmissions: %d\n', ...
@@ -228,9 +221,9 @@ fprintf('Gossip (Average Consensus) - Convergence time: %d iterations, Transmiss
 x_pdmm_avg = x_initial;
 degree_per_node = sum(adjacency_matrix, 2); % degree of each node
 
-% dual variables z_{i|j}, z_{j|i}
-z = zeros(num_sensors); % z(i,j) = z_{i|j}
-y = zeros(num_sensors); % temporary - y_{i|j}
+% auxiliary variables
+z_pdmm_avg = zeros(num_sensors); % z(i,j) = z_{i|j}
+y_pdmm_avg = zeros(num_sensors); % temporary - y_{i|j}
 c = 1 / max(degree_per_node); % hyperparameter
 
 error_history_pdmm_avg = zeros(max_iterations, 1);
@@ -262,7 +255,7 @@ for iter_pdmm_avg = 1:max_iterations
     for i = 1:num_sensors
         sum_z = 0;
         for j = find(adjacency_matrix(i,:))
-            sum_z = sum_z + A(i,j) * z(i,j);
+            sum_z = sum_z + A(i,j) * z_pdmm_avg(i,j);
         end
         x_new(i) = (x_initial(i) - sum_z) / (1 + c * degree_per_node(i));
     end
@@ -270,14 +263,14 @@ for iter_pdmm_avg = 1:max_iterations
     % y update - y_{i|j} = z_{i|j} + 2c * A_{ij} * x_i
     for i = 1:num_sensors
         for j = find(adjacency_matrix(i,:))
-            y(i,j) = z(i,j) + 2 * c * A(i,j) * x_new(i);
+            y_pdmm_avg(i,j) = z_pdmm_avg(i,j) + 2 * c * A(i,j) * x_new(i);
         end
     end
 
     % z update - z_{j|i} = y_{i|j}
     for i = 1:num_sensors
         for j = find(adjacency_matrix(i,:))
-            z(i,j) = y(j,i);
+            z_pdmm_avg(i,j) = y_pdmm_avg(j,i);
         end
     end
 
@@ -289,129 +282,108 @@ if iter_pdmm_avg == max_iterations
     fprintf("WARNING: Maximum iterations reached without convergence!\n");
 end
 
+transmissions_per_iter = 2 * sum(degree_per_node);
+transmissions_axis_pdmm_avg = transmissions_per_iter * (1:length(error_history_pdmm_avg));
+transmissions_pdmm_avg = (convergence_time_pdmm_avg - 1) * transmissions_per_iter;
+
 % plotting convergence
 figure;
-semilogy(1:size(error_history_pdmm_avg,1), error_history_pdmm_avg);
+semilogy(transmissions_axis_pdmm_avg, error_history_pdmm_avg);
 title('PDMM Convergence (Average Consensus)');
-xlabel('Iterations'); ylabel('MSE w.r.t. True Average');
+xlabel('Transmissions'); ylabel('MSE w.r.t. True Average');
 grid on;
-
-transmissions_per_iter = sum(degree_per_node);
-transmissions_pdmm_avg = convergence_time_pdmm_avg * transmissions_per_iter;
 
 fprintf('PDMM (Average Consensus) - Convergence time: %d iterations, Transmissions: %d\n', ...
             convergence_time_pdmm_avg, transmissions_pdmm_avg);
 
 % convergence comparison with randomized gossip
 figure;
-semilogy(1:size(error_history_gossip,1), error_history_gossip);
+semilogy(transmission_axis_gossip, error_history_gossip);
 hold on;
-semilogy(1:size(error_history_pdmm_avg,1), error_history_pdmm_avg);
+semilogy(transmissions_axis_pdmm_avg, error_history_pdmm_avg);
 title(sprintf('Average Consensus on RGG - Convergence Comparison(r = %d, n = %d)', comm_radius, num_sensors));
-xlabel('Iterations'); ylabel('MSE w.r.t. True Average');
+xlabel('Transmissions'); ylabel('MSE w.r.t. True Average');
 legend('Randomized Gossip', 'PDMM');
 grid on;
 
-
-
-
-
-
-
-%PDMM median:
-
 %% PDMM implementation (median consensus)
-x_pdmm_median = x_initial;
-degree_per_node = sum(adjacency_matrix, 2); % degree of each node
+true_median = median(x_initial);
+x_pdmm_med = x_initial;
+tolerance_med = 1e-9;
+max_iterations_median = max_iterations / 50;
 
-% dual variables z_{i|j}, z_{j|i}
-z = zeros(num_sensors); % z(i,j) = z_{i|j}
-y = zeros(num_sensors); % temporary - y_{i|j}
-c = 1 / max(degree_per_node); % hyperparameter
+error_history_pdmm_med = zeros(max_iterations_median, 1);
+convergence_time_pdmm_med = 0;
 
-error_history_pdmm_median = zeros(max_iterations, 1);
-convergence_time_pdmm_median = 0;
+% auxiliary variables
+z_pdmm_med = zeros(num_sensors); % z(i,j) = z_{i|j}
+y_pdmm_med = zeros(num_sensors); % temporary - y_{i|j}
 
-% A-weights per edge pair - stored using adjacency matrix structure
-A = adjacency_matrix;
-A(triu(true(size(A)), 1)) = -A(triu(true(size(A)), 1));
-
-for iter_pdmm_median = 1:max_iterations
-    error_history_pdmm_median(iter_pdmm_median) = norm(x_pdmm_median - true_median);
+for iter_pdmm_med = 1:max_iterations_median
+    error_history_pdmm_med(iter_pdmm_med) = norm(x_pdmm_med - true_median);
 
     % convergence check
-    if iter_pdmm_median > 1 && norm(x_pdmm_median - true_median) < tolerance
-        convergence_time_pdmm_median = iter_pdmm_median;
-        error_history_pdmm_median = error_history_pdmm_median(1:iter_pdmm_median, :);
+    if iter_pdmm_med > 1 && norm(x_pdmm_med - true_median) < tolerance_med
+        convergence_time_pdmm_med = iter_pdmm_med;
+        error_history_pdmm_med = error_history_pdmm_med(1:iter_pdmm_med, :);
         break;
     end
 
-    if iter_pdmm_median > 1 && norm(x_pdmm_median - true_median) > 1e3
+    if iter_pdmm_med > 1 && norm(x_pdmm_med - true_median) > 1e3
         fprintf("Graph diverging!!!");
-        convergence_time_pdmm_median = iter_pdmm_median;
-        error_history_pdmm_median = error_history_pdmm_median(1:iter_pdmm_median, :);
+        convergence_time_pdmm_med = iter_pdmm_med;
+        error_history_pdmm_med = error_history_pdmm_med(1:iter_pdmm_med, :);
         break;
     end
-
-
-
-
 
     % primal update
     x_new = zeros(num_sensors, 1);
     for i = 1:num_sensors
         sum_z = 0;
         for j = find(adjacency_matrix(i,:))
-            sum_z = sum_z + A(i,j) * z(i,j);
+            sum_z = sum_z + A(i,j) * z_pdmm_med(i,j);
         end
-        %median update here now:
-
-
-        gamma = (-sum_z) / (degree_per_node(i) * c); 
-
-        if gamma - 1/(degree_per_node(i) * c) > x_initial(i)      %this weird expression as sign is not invertible
-            x_new(i) = gamma -1/(degree_per_node(i) * c);
-        elseif (gamma + 1 / (degree_per_node(i) * c))  < x_initial(i)
-            x_new(i) = gamma + 1 / (degree_per_node(i) * c);
-        else    
+        if (-1 - sum_z) / (c * degree_per_node(i)) > x_initial(i)
+            x_new(i) = (-1 - sum_z) / (c * degree_per_node(i));
+        elseif (1 - sum_z) / (c * degree_per_node(i)) < x_initial(i)
+            x_new(i) = (1 - sum_z) / (c * degree_per_node(i));
+        else
             x_new(i) = x_initial(i);
         end
-
     end
 
     % y update - y_{i|j} = z_{i|j} + 2c * A_{ij} * x_i
     for i = 1:num_sensors
         for j = find(adjacency_matrix(i,:))
-            y(i,j) = z(i,j) + 2 * c * A(i,j) * x_new(i);
+            y_pdmm_med(i,j) = z_pdmm_med(i,j) + 2 * c * A(i,j) * x_new(i);
         end
     end
 
-    % z update - z_{j|i} = y_{i|j}
+    % z update - z_{i|j} = 0.5z_{i|j} + 0.5y_{j|i}
     for i = 1:num_sensors
         for j = find(adjacency_matrix(i,:))
-            z(i,j) = 0.5*(z(i,j) + y(j,i));   %half averaging
+            z_pdmm_med(i,j) = 0.5 * z_pdmm_med(i,j) + 0.5 * y_pdmm_med(j,i);
+            % z_pdmm_med(i,j) = y_pdmm_med(j,i);
         end
     end
 
-    x_pdmm_median = x_new;
+    x_pdmm_med = x_new;
 end
 
-
-
-if iter_pdmm_median == max_iterations
-    convergence_time_pdmm_median = iter_pdmm_median;
+if iter_pdmm_med == max_iterations_median
+    convergence_time_pdmm_med = iter_pdmm_med;
     fprintf("WARNING: Maximum iterations reached without convergence!\n");
 end
 
+transmissions_axis_pdmm_med = transmissions_per_iter * (1:length(error_history_pdmm_med));
+transmissions_pdmm_med = (convergence_time_pdmm_med - 1) * transmissions_per_iter;
+
 % plotting convergence
 figure;
-semilogy(1:size(error_history_pdmm_median,1), error_history_pdmm_median);
+semilogy(transmissions_axis_pdmm_med, error_history_pdmm_med);
 title('PDMM Convergence (Median Consensus)');
-xlabel('Iterations'); ylabel('MSE w.r.t. True Median');
+xlabel('Transmissions'); ylabel('MSE w.r.t. True Median');
 grid on;
 
-transmissions_per_iter = sum(degree_per_node);
-transmissions_pdmm_median = convergence_time_pdmm_median * transmissions_per_iter;
-
 fprintf('PDMM (Median Consensus) - Convergence time: %d iterations, Transmissions: %d\n', ...
-            convergence_time_pdmm_median, transmissions_pdmm_median);
+            convergence_time_pdmm_med, transmissions_pdmm_med);
